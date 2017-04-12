674e94e4f7bf17594957385e7a89558e4c175fbd
Hack Reactor Student <student@hackreactor.com>
2017-04-12 15:14:35 -0700

    Gorcery

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-// TODO
+var GroceryList = (props) => (
+  <div>
+    <h2>Grocery Time</h2>
+    {props.items.map(item => 
+      <GroceryListItems item={item} /> 
+    )}
+  </div>
+);
+
+
+class GroceryListItems extends React.Component {
+
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <li>{this.props.item}</li>
+      );
+  }
+  
+  onListItemClick (event) {
+    console.log('I got clicked');
+  };
+
+};
+
+
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
+  // return ( 
+  // <ul>
+  //   <li onClick={onListItemClick}>{props.items[0]}</li>
+  //   <li>{props.items[1]}</li>
+  //   <li>{props.items[2]}</li>
+  // </ul>
